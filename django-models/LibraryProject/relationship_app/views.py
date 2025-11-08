75c12462b6a93c7891d742a87fa08bf86fbd57d6
mtsdb <tomatias.te@gmail.com>
2025-11-08 21:37:19 +0300

    1. Django Views and URL Configuration update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,6 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
