beb868113cdc781257a7c5749b03550d387ab146
mtsdb <tomatias.te@gmail.com>
2025-11-08 22:17:03 +0300

    4. Implementing Custom Permissions in Django 0.2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
     
-    path('book/add/', add_book, name='add_book'),
-    path('book/edit/<int:book_id>/', edit_book, name='edit_book'),
-    path('book/delete/<int:book_id>/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
