d4a10179c3fdaf464e8c9f3886e209a3eee4eb26
mtsdb <tomatias.te@gmail.com>
2025-11-13 23:39:50 +0300

    0. Implementing a Custom User Model in Django update 01

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,47 +1,38 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth import get_user_model
 from .models import Book
+from .models import CustomUser
+from django.apps import AppConfig
+from django.contrib.auth.admin import UserAdmin
+
+
+class BookshelfConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bookshelf'
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
 
-CustomUser = get_user_model()
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active']
     fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "date_of_birth", "profile_photo")}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                ),
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
-
     add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo"),
-            },
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
 
-    list_display = ("username", "email", "first_name", "last_name", "is_staff")
-    search_fields = ("username", "first_name", "last_name", "email")
-    ordering = ("username",)
+admin.site.register(CustomUser, CustomUserAdmin)
