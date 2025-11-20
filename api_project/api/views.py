f72686463b5b12d6376525f03aae10c457225d3e
mtsdb <tomatias.te@gmail.com>
2025-11-20 16:23:03 +0300

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework import permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,6 +10,14 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    """ViewSet that provides CRUD operations for the Book model."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+    def get_permissions(self):
+        if self.request.method == 'DELETE':
+            permission_classes = [permissions.IsAdminUser]
+        elif self.request.method in permissions.SAFE_METHODS:
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
