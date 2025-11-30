0ca0cc2ab94b6a6295b31eae98664f131173ff20
mtsdb <tomatias.te@gmail.com>
2025-11-30 09:48:02 +0300

    1. Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookListView(generics.ListAPIView):
+
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [AllowAny]
+
+	def get_queryset(self):
+		qs = super().get_queryset()
+		year = self.request.query_params.get("year")
+		if year:
+			try:
+				year_int = int(year)
+				qs = qs.filter(publication_year=year_int)
+			except (TypeError, ValueError):
+				pass
+		return qs
+
+
+class BookDetailView(generics.RetrieveAPIView):
+
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [AllowAny]
+
+
+class BookCreateView(generics.CreateAPIView):
+
+
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [IsAuthenticated]
+
+	def perform_create(self, serializer: BookSerializer) -> None:
+
+		serializer.save()
+
+
+class BookUpdateView(generics.UpdateAPIView):
+
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [IsAuthenticated]
+
+	def perform_update(self, serializer: BookSerializer) -> None:
+		serializer.save()
+
+
+class BookDeleteView(generics.DestroyAPIView):
+
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [IsAuthenticated]
+
