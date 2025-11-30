088e7d3c6d565bd731e96d7b670aef3e1b52d365
mtsdb <tomatias.te@gmail.com>
2025-11-30 09:39:18 +0300

    0. Setting Up a New Django Project with Custom Serializers in Django REST Framework

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Author(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __str__(self) -> str: 
+		return self.name
+
+
+
+class Book(models.Model):
+
+	title = models.CharField(max_length=300)
+	publication_year = models.IntegerField()
+	author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+	def __str__(self) -> str:
+		return f"{self.title} ({self.publication_year})"
+
