a701d6957ff5c91e6e530413044521fb1d0deb9d
mtsdb <tomatias.te@gmail.com>
2025-12-07 14:16:34 +0300

    Refactor Post model to use User directly instead of AUTH_USER_MODEL

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -7,7 +7,7 @@ class Post(models.Model):
 	content = models.TextField()
 	published_date = models.DateTimeField(auto_now_add=True)
 	author = models.ForeignKey(
-		settings.AUTH_USER_MODEL,
+		User,
 		on_delete=models.CASCADE,
 		related_name="posts",
 	)
